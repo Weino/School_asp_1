9c69aa93ed9785e0165acb4e0ad028c9136bba01
william weinö <william.weino@hotmail.com>
2022-01-13 19:59:05 +0100

    fixed my events and the navbar

--- Models/AttendeeEvent.cs
+++ Models/AttendeeEvent.cs
@@ -2,10 +2,7 @@
 {
     public class AttendeeEvent
     {
-        public int AttendeeEventId { get; set; }
-        public int AttendeeId { get; set; }
-        public int EventId { get; set; }
-
+        public int Id { get; set; }
         public Attendee Attendee { get; set; }
         public Event Event { get; set; }
     }
---
+++ Pages/MyEvents.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using asp_assign_1.Data;
+using asp_assign_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp_assign_1.Pages
+{
+    public class MyEventsModel : PageModel
+    {
+        private readonly EventDbContext _ctx;
+
+        public MyEventsModel(EventDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IList<AttendeeEvent> AttendeeEvent {  get; set; }
+        public async void OnGetAsync()
+        {
+                AttendeeEvent = await _ctx.AttendeeEvents
+                .Include(r => r.Event)
+                .Include(r => r.Attendee).ToListAsync(); 
+        }
+    }
+}
